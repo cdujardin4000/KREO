f226af7e735263114a16dc29b46466ced8c5f000
cdujardin4000 <cdujardin4000@gmail.com>
2020-02-23 14:34:17 +0100

    menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 const   menu        =   document.querySelector('.l-main-menu');
-
-
+//var     items       =   document.querySelectorAll('.secHome', '.secWorks', '.secServices', '.secAbout', '.secContact');
+//items.addEventListener('click', e => {
+//    menu.classList.toggle('open');
+//});
 document.querySelectorAll('.nav-hamburger').forEach(btn => {
     btn.addEventListener('click', e => {
         btn.classList.toggle('active');
         menu.classList.toggle('open');
     });
+    
 });
 
 
